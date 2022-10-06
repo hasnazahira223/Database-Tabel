780ec07b70b93cf8413291ce125f8495d45eaca5
hasnazahira223 <106467896+hasnazahira223@users.noreply.github.com>
2022-10-06 21:57:52 +0700

    Add files via upload

---
+++ feb/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from . models import Dosen, Tenaga_Pendidik, Mahasiswa
+
+# Create your views here.
+
+def feb(request):
+    dosen = Dosen.objects.all()
+    tenagaPendidik = Tenaga_Pendidik.objects.all()
+    mahasiswa = Mahasiswa.objects.all()
+
+    context = {
+        'dataDosen': dosen,
+        'dataTenaga_Pendidik': tenagaPendidik,
+        'dataMahasiswa': mahasiswa,
+    }
+    return render(request, 'indexfeb.html', context)
